699e8b4be99141614568a93c7b763cbc1a36c474
Cookiecoolkid <1766462664@qq.com>
2024-12-23 11:04:03 +0800

    modify indent format

--- fsrecov/fsrecov.c
+++ fsrecov/fsrecov.c
@@ -1,13 +1,12 @@
-#include <stdio.h>
-#include <string.h>
-#include <stdbool.h>
-#include <stdlib.h>
-#include <sys/mman.h>
-#include <fcntl.h>
-#include <unistd.h>
-#include <assert.h>
 #include "fat32.h"
-
+#include <assert.h>
+#include <fcntl.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/mman.h>
+#include <unistd.h>
 
 #pragma pack(push, 1)
 typedef struct {
@@ -67,20 +66,22 @@ typedef struct {
 
 #define ATTR_LONG_NAME (0x0F)
 
-void readDirEntry(char* data, int clusterSize) {
-    DirEntry* de = (DirEntry*)data;
-    LFNEntry* lfn;
+void readDirEntry(char *data, int clusterSize) {
+    DirEntry *de = (DirEntry *)data;
+    LFNEntry *lfn;
     wchar_t fileName[256];
     char tempFileName[256];
-		int pos = 0;
+    int pos = 0;
 
     for (int i = 0; i < clusterSize / sizeof(DirEntry); ++i, ++de) {
-        if (de->name[0] == 0x00) break;
-        if (de->name[0] == 0xe5) continue;
+        if (de->name[0] == 0x00)
+            break;
+        if (de->name[0] == 0xe5)
+            continue;
 
         if (de->attr == ATTR_LONG_NAME) {
-            lfn = (LFNEntry*)de;
-						
+            lfn = (LFNEntry *)de;
+
             memcpy(&fileName[pos], lfn->name1, 5 * sizeof(uint16_t));
             pos += 5;
             memcpy(&fileName[pos], lfn->name2, 6 * sizeof(uint16_t));
@@ -88,22 +89,21 @@ void readDirEntry(char* data, int clusterSize) {
             memcpy(&fileName[pos], lfn->name3, 2 * sizeof(uint16_t));
             pos += 2;
 
-
-						if ((lfn->seqNumber & 0x40) == 0x40) {
-								fileName[pos] = L'\0';
-								pos = 0;
+            if ((lfn->seqNumber & 0x40) == 0x40) {
+                fileName[pos] = L'\0';
+                pos = 0;
                 wcstombs(tempFileName, fileName, 256);
-								printf("File: %s\n", tempFileName);
+                printf("File: %s\n", tempFileName);
 
-								memset(fileName, 0, sizeof(fileName));
-						}
+                memset(fileName, 0, sizeof(fileName));
+            }
         } else if (!(de->attr & ATTR_VOLUME_ID)) {
             memcpy(fileName, de->name, 11 * sizeof(uint16_t));
             fileName[11] = L'\0';
-						wcstombs(tempFileName, fileName, 256);
-						printf("File: %s\n", tempFileName);
-	
-						memset(fileName, 0, sizeof(fileName));
+            wcstombs(tempFileName, fileName, 256);
+            printf("File: %s\n", tempFileName);
+
+            memset(fileName, 0, sizeof(fileName));
         }
     }
 }
@@ -113,132 +113,137 @@ int clusterCategory[MAX_CLUSTERS];
 int cluster_size = 0;
 int cluster_num = 0;
 
-void *mmap_fat32img(const char* fsimg) {
-		int fd = open(fsimg, O_RDONLY);
-		if (fd == -1) {
-				perror("Error open fsimg");
-				return NULL;
-		}
-		
-		off_t size = lseek(fd, 0, SEEK_END);
+void *mmap_fat32img(const char *fsimg) {
+    int fd = open(fsimg, O_RDONLY);
+    if (fd == -1) {
+        perror("Error open fsimg");
+        return NULL;
+    }
 
-		struct fat32hdr *hdr = mmap(NULL, size, PROT_READ, MAP_PRIVATE, fd, 0);
+    off_t size = lseek(fd, 0, SEEK_END);
+
+    struct fat32hdr *hdr = mmap(NULL, size, PROT_READ, MAP_PRIVATE, fd, 0);
     if (hdr == MAP_FAILED) {
         perror("Error mapping file");
         close(fd);
         return NULL;
     }
-		
-		close(fd);
 
-		assert(hdr->Signature_word == 0xaa55); // this is an MBR
-		assert(hdr->BPB_TotSec32 * hdr->BPB_BytsPerSec == size);
+    close(fd);
 
-		/*	
-		printf("Tot Clus = %d\n", hdr->BPB_TotSec32 / hdr->BPB_SecPerClus);	
-		printf("%s: DOS/MBR boot sector, ", fsimg);
-		printf("OEM-ID \"%s\", ", hdr->BS_OEMName);
-		printf("sectors/cluster %d, ", hdr->BPB_SecPerClus);
-		printf("sectors %d, ", hdr->BPB_TotSec32);
-		printf("sectors %d, ", hdr->BPB_TotSec32);
-		printf("sectors/FAT %d, ", hdr->BPB_FATSz32);
-		printf("serial number 0x%x\n", hdr->BS_VolID);
-		*/
-		return (void*)hdr;
+    assert(hdr->Signature_word == 0xaa55); // this is an MBR
+    assert(hdr->BPB_TotSec32 * hdr->BPB_BytsPerSec == size);
+
+    /*
+    printf("Tot Clus = %d\n", hdr->BPB_TotSec32 / hdr->BPB_SecPerClus);
+    printf("%s: DOS/MBR boot sector, ", fsimg);
+    printf("OEM-ID \"%s\", ", hdr->BS_OEMName);
+    printf("sectors/cluster %d, ", hdr->BPB_SecPerClus);
+    printf("sectors %d, ", hdr->BPB_TotSec32);
+    printf("sectors %d, ", hdr->BPB_TotSec32);
+    printf("sectors/FAT %d, ", hdr->BPB_FATSz32);
+    printf("serial number 0x%x\n", hdr->BS_VolID);
+    */
+    return (void *)hdr;
 }
 
-
 u32 next_cluster(u32 n) {
-		u32 FATOffset = 4 * n; // FAT32
-		u32 FATSecNum = hdr->BPB_RsvdSecCnt + (FATOffset / hdr->BPB_BytsPerSec);
-		u32 FATEntOffset = FATOffset % hdr->BPB_BytsPerSec;
+    u32 FATOffset = 4 * n; // FAT32
+    u32 FATSecNum = hdr->BPB_RsvdSecCnt + (FATOffset / hdr->BPB_BytsPerSec);
+    u32 FATEntOffset = FATOffset % hdr->BPB_BytsPerSec;
 
-		u8 *SecBuff = (u8 *)hdr + FATSecNum * hdr->BPB_BytsPerSec;
-		// DWARD - u32
-		u32 FATClusEntryVal = (*((u32 *) &SecBuff[FATEntOffset])) & 0x0FFFFFFF;
+    u8 *SecBuff = (u8 *)hdr + FATSecNum * hdr->BPB_BytsPerSec;
+    // DWARD - u32
+    u32 FATClusEntryVal = (*((u32 *)&SecBuff[FATEntOffset])) & 0x0FFFFFFF;
 
-		return FATClusEntryVal;
+    return FATClusEntryVal;
 }
 
 void *clus2sec(u32 n) {
-		u32 RootDirSectors = ((hdr->BPB_RootEntCnt * 32) + (hdr->BPB_BytsPerSec - 1)) / hdr->BPB_BytsPerSec;
-		u32 FirstDataSec = hdr->BPB_RsvdSecCnt + (hdr->BPB_NumFATs * hdr->BPB_FATSz32) + RootDirSectors;
+    u32 RootDirSectors =
+        ((hdr->BPB_RootEntCnt * 32) + (hdr->BPB_BytsPerSec - 1)) /
+        hdr->BPB_BytsPerSec;
+    u32 FirstDataSec = hdr->BPB_RsvdSecCnt +
+                       (hdr->BPB_NumFATs * hdr->BPB_FATSz32) + RootDirSectors;
 
-		// Clus#1 Clus#2 not for data
-		u32 FirstDataSecOfClus = FirstDataSec + (n - 2) * hdr->BPB_SecPerClus;
-		return (void *)((char *)hdr + (FirstDataSecOfClus * hdr->BPB_BytsPerSec));
+    // Clus#1 Clus#2 not for data
+    u32 FirstDataSecOfClus = FirstDataSec + (n - 2) * hdr->BPB_SecPerClus;
+    return (void *)((char *)hdr + (FirstDataSecOfClus * hdr->BPB_BytsPerSec));
 }
 
-bool is_all_zeros(const char* buffer, size_t size) {
-    for (size_t i = 0; i < size; ++i) if (buffer[i] != 0) return false;
-    
-		return true;
+bool is_all_zeros(const char *buffer, size_t size) {
+    for (size_t i = 0; i < size; ++i)
+        if (buffer[i] != 0)
+            return false;
+
+    return true;
 }
 
 int count_cluster_bmp(u32 clusId) {
-    char *data = (char*)clus2sec(clusId);
+    char *data = (char *)clus2sec(clusId);
     int count = 0;
 
-    for (int i = 0; i < cluster_size - 2; i++) 
-        if (data[i] == 'B' && data[i + 1] == 'M' && data[i + 2] == 'P') count++;        
- 
-    return count; 
+    for (int i = 0; i < cluster_size - 2; i++)
+        if (data[i] == 'B' && data[i + 1] == 'M' && data[i + 2] == 'P')
+            count++;
+
+    return count;
 }
 
 int check_cluster_type(u32 clusId) {
-    char *data = (char*)clus2sec(clusId);
+    char *data = (char *)clus2sec(clusId);
     if (is_all_zeros(data, cluster_size)) {
-				// printf("Find Unused\n");
-				return UNUSED;
-		}
+        // printf("Find Unused\n");
+        return UNUSED;
+    }
     if (data[0] == 0x42 && data[1] == 0x4D) {
-				// printf("Find BmpHeader\n");
-				return BMPHDR;
-		}
-		int bmpFileCount = count_cluster_bmp(clusId);
+        // printf("Find BmpHeader\n");
+        return BMPHDR;
+    }
+    int bmpFileCount = count_cluster_bmp(clusId);
 
     if (bmpFileCount > 1) {
-				// printf("Find Directory\n");
+        // printf("Find Directory\n");
         return DIRECTORY;
     }
-		// printf("Find BmpData\n");
+    // printf("Find BmpData\n");
     return BMPDATA;
 }
 
 void TraverseClus() {
-		for (int clusId = 2; clusId < cluster_num; clusId += 1) {
-				assert(clusId <= MAX_CLUSTERS);
-				clusterCategory[clusId] = check_cluster_type(clusId);
-		}
+    for (int clusId = 2; clusId < cluster_num; clusId += 1) {
+        assert(clusId <= MAX_CLUSTERS);
+        clusterCategory[clusId] = check_cluster_type(clusId);
+    }
 }
 
-void TraverseDirectory() {                                                             
-		for (int clusId = 2; clusId < cluster_num; clusId += 1) {
-				if (clusterCategory[clusId] == DIRECTORY) {
-						char* data;
-						while (clusId < CLUS_INVALID) {
-								data = (char*)clus2sec(clusId);
-								readDirEntry(data, cluster_size);
-								clusId = next_cluster(clusId); 
-						}
-				}
-		}
+void TraverseDirectory() {
+    for (int clusId = 2; clusId < cluster_num; clusId += 1) {
+        if (clusterCategory[clusId] == DIRECTORY) {
+            char *data;
+            while (clusId < CLUS_INVALID) {
+                data = (char *)clus2sec(clusId);
+                readDirEntry(data, cluster_size);
+                clusId = next_cluster(clusId);
+            }
+        }
+    }
 }
-
 
 int main(int argc, char *argv[]) {
-		if (argc < 2) {
-				fprintf(stderr, "Usage: %s fs-image\n", argv[0]);
-				exit(1);
-		}
+    if (argc < 2) {
+        fprintf(stderr, "Usage: %s fs-image\n", argv[0]);
+        exit(1);
+    }
 
-		hdr = (struct fat32hdr*)mmap_fat32img(argv[1]);
+    hdr = (struct fat32hdr *)mmap_fat32img(argv[1]);
 
-		cluster_size = hdr->BPB_BytsPerSec * hdr->BPB_SecPerClus;
-  	cluster_num = (hdr->BPB_TotSec32 - hdr->BPB_RsvdSecCnt - (hdr->BPB_NumFATs * hdr->BPB_FATSz32)) / hdr->BPB_SecPerClus; 
-		TraverseClus();
-		TraverseDirectory();
-				
-		return 0;
+    cluster_size = hdr->BPB_BytsPerSec * hdr->BPB_SecPerClus;
+    cluster_num = (hdr->BPB_TotSec32 - hdr->BPB_RsvdSecCnt -
+                   (hdr->BPB_NumFATs * hdr->BPB_FATSz32)) /
+                  hdr->BPB_SecPerClus;
+    TraverseClus();
+    TraverseDirectory();
+
+    return 0;
 }
-
--- libco/co.c
+++ libco/co.c
@@ -1,10 +1,9 @@
 #include "co.h"
-#include <stdlib.h>
-#include <ucontext.h>
 #include <assert.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <time.h>
-
+#include <ucontext.h>
 
 #ifdef DEBUG
 #define debug(...) printf(__VA_ARGS__)
@@ -16,84 +15,84 @@
 #define COSIZE 128
 
 struct co {
-		char stk[STKSIZE];
-		struct ucontext_t uctx;
-		int idx;
-		// void (*func)(void *);
-		// void *arg;
+    char stk[STKSIZE];
+    struct ucontext_t uctx;
+    int idx;
+    // void (*func)(void *);
+    // void *arg;
 };
 
-struct co* co_threads[COSIZE];
-struct co* cur;
+struct co *co_threads[COSIZE];
+struct co *cur;
 ucontext_t main_ctx;
-int main_ctx_init = 0; 
+int main_ctx_init = 0;
 
-struct co* co_start(const char *name, void (*func)(void *), void *arg) {
-		struct co* co_t = (struct co*)malloc(sizeof(struct co));
-		assert(co_t);
+struct co *co_start(const char *name, void (*func)(void *), void *arg) {
+    struct co *co_t = (struct co *)malloc(sizeof(struct co));
+    assert(co_t);
 
-		int idx = 0;
-		while (idx < 128 && co_threads[idx] != NULL) idx++;
-		assert(idx != 128);
+    int idx = 0;
+    while (idx < 128 && co_threads[idx] != NULL)
+        idx++;
+    assert(idx != 128);
 
-		co_t->idx = idx;
-		co_threads[idx] = co_t;
-		
-		debug("co_threads[%d] = %p\n", idx, co_threads[idx]);
+    co_t->idx = idx;
+    co_threads[idx] = co_t;
 
-		getcontext(&(co_t->uctx));
-		co_t->uctx.uc_stack.ss_sp = co_t->stk;
-		co_t->uctx.uc_stack.ss_size = sizeof(co_t->stk);
-		
+    debug("co_threads[%d] = %p\n", idx, co_threads[idx]);
+
+    getcontext(&(co_t->uctx));
+    co_t->uctx.uc_stack.ss_sp = co_t->stk;
+    co_t->uctx.uc_stack.ss_size = sizeof(co_t->stk);
+
     if (!main_ctx_init) {
         getcontext(&main_ctx);
         main_ctx_init = 1;
     }
-		if (cur) co_t->uctx.uc_link = &main_ctx; // (cur->ctx)
-		else 
-		{
-				co_t->uctx.uc_link = &main_ctx;  // NULL
-		}
-		int argc = 0;
-		char* args = (char*)arg;
-		
-		while(args[argc]) 
-		{
-				debug("args[%d] = %c\n", argc, args[argc]);
-				argc++;
-		}
-		debug("argc:%d\n", argc);
+    if (cur)
+        co_t->uctx.uc_link = &main_ctx; // (cur->ctx)
+    else {
+        co_t->uctx.uc_link = &main_ctx; // NULL
+    }
+    int argc = 0;
+    char *args = (char *)arg;
 
-		makecontext(&(co_t->uctx), (void*)func, argc, args);
+    while (args[argc]) {
+        debug("args[%d] = %c\n", argc, args[argc]);
+        argc++;
+    }
+    debug("argc:%d\n", argc);
 
-		return co_t;
+    makecontext(&(co_t->uctx), (void *)func, argc, args);
+
+    return co_t;
 }
 
 void co_wait(struct co *co) {
-		cur = co;
-		// setcontext(&co->uctx);
-		swapcontext(&main_ctx, &co->uctx);
-		co_threads[co->idx] = NULL;
-		free(co);
+    cur = co;
+    // setcontext(&co->uctx);
+    swapcontext(&main_ctx, &co->uctx);
+    co_threads[co->idx] = NULL;
+    free(co);
 }
 
-void co_yield() {
-		int idx = (cur->idx + 1) % COSIZE; 
+void co_yield () {
+    int idx = (cur->idx + 1) % COSIZE;
 
-		while(idx != cur->idx && co_threads[idx] == NULL) 
-				idx = (idx + 1) % COSIZE;
+    while (idx != cur->idx && co_threads[idx] == NULL)
+        idx = (idx + 1) % COSIZE;
 
-		if (co_threads[idx] == NULL) return;
+    if (co_threads[idx] == NULL)
+        return;
 
+    // debug("Switch to %d\n", idx);
 
-		// debug("Switch to %d\n", idx);
-		
-		struct co* prev = cur; 
-		// getcontext(&(cur->uctx));	
-		assert(co_threads[idx]);	
-	
-		// co_threads[idx]->uctx.uc_link = &(cur->uctx);
-		cur = co_threads[idx];
-		swapcontext(&(prev->uctx), &(cur->uctx));
-		// setcontext(&(co_threads[idx]->uctx));
+    struct co *prev = cur;
+    // getcontext(&(cur->uctx));
+    assert(co_threads[idx]);
+
+    // co_threads[idx]->uctx.uc_link = &(cur->uctx);
+    cur = co_threads[idx];
+    swapcontext(&(prev->uctx), &(cur->uctx));
+    // setcontext(&(co_threads[idx]->uctx));
 }
